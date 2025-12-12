a77736507dcf0a84a827cdf20e99c8eec5c6067e
oliv3a <98859264+oliv3a@users.noreply.github.com>
2025-12-12 17:19:09 +0800

    Add greet() function on feature-1 branch

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ print('hello world')
 print("Learning Git is fun!")
 print("Branching is awesome!")
 print("Practicing on feature-1 branch!")
+def greet(name):
+    print(f"Hello, {name}! Welcome to feature-1 branch.")
+
+greet("Git Learner")
